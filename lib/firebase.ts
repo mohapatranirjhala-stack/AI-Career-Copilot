29666ff4d596ac5e96e2105641af277ef317f7c8
Nirjhala Mohapatra <nirjhala.23bsa10052@vitbhopal.ac.in>
2026-06-15 11:16:20 +0530

    Fixed auth export

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey:
@@ -26,3 +27,4 @@ const app = initializeApp(firebaseConfig);
 
 export const db =
   getFirestore(app);
+  export const auth = getAuth(app)
